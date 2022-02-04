9829ba9216ffe6de02fd031559c3ecadfdd2eeab
Connie Bernardin <conniebernardin@googlemail.com>
2022-02-04 17:12:54 +0000

    creating addCar method

--- src/ObjectOrientedProgramming/ClassesExercises/ExerciseOne/carService.java
+++ src/ObjectOrientedProgramming/ClassesExercises/ExerciseOne/carService.java
@@ -4,7 +4,6 @@ public class carService {
     //method to count cars in CarDealership. Takes no parameters, returns an int.
     public int carCount(CarDealership carDealership){
         int carsCounted = 0;
-
         for (Car c : carDealership.getInStock()) {
             if (c != null){
                 carsCounted ++;
@@ -12,4 +11,14 @@ public class carService {
         }
         return carsCounted;
     }
+
+    public int addCar(CarDealership carDealership, Car car){
+
+        for (int i = 0; i < carDealership.getInStock().length; i++) { //loop through array InStock from carDealership class
+            if (carDealership.getInStock()[i] == null) { //if index i is equal to null, there is a space. You can then add car.
+                carDealership.getInStock()[i] = car; // car added to index point i
+                break;
+
+            }
+        }
 }
